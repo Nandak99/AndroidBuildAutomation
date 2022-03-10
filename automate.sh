72544b51f34b3a1535e86943c0ceca9d28e82684
Nandak99 <54415494+Nandak99@users.noreply.github.com>
2022-03-10 17:56:52 +0530

    Update automate.sh

--- automate.sh
+++ automate.sh
@@ -1,7 +1,7 @@
 
 #Define all paths, constants here
-PROJECT_DIR='env.WORKSPACE/'
-OUTPUT_DIR='env.WORKSPACE/'
+PROJECT_DIR='${WORKSPACE}/'
+OUTPUT_DIR='${WORKSPACE}/'
 
 # Functions for customizing colors(Optional)
 print_red(){
